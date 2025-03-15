4a6600b208c3a3d24c51b2e79abea09170b9beee
robertapierre <202501066526@alunos.estacio.br>
2025-03-15 19:40:55 +0000

    novo comando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,20 +3,23 @@ int main() {
  
    //Variáveis para a primeira carta.
    char estado01 ='Ceará';
-   char codigo01[4]= 'A01';
-   char cidade01[50]= 'Fortaleza' ;
+   char codigo01[4]= "A01";
+   char cidade01[50]= "Fortaleza";
    int populacao01 = 2400000;
    float area01 = 312353;
-   float 
+   float pib01 = 73000000;
+   int pontos_turisticos01= 50;
 
-  
+
 
    //Váriaveis para a segunda carta.
-   char estado02 = 'B';
-   char codigo02[4] = 'B02';
-   char cidade02[50];
-   int populacao02, pontos_turisticos02;
-   float area02, pib02;
+   char estado02 = 'Bahia';
+   char codigo02[4] = "B02";
+   char cidade02[50]= "Salvador";
+   int populacao02= 2000000;
+   float area02= 693000;
+   float pib02= 62000000;
+   int pontos_turisticos02= 40;
 
    //Solicitação dos dados para o usuário, primeira carta.
    printf ("Digite os dados da primeira carta:");
@@ -25,10 +28,10 @@ int main() {
    scanf ("%c", &estado01);
 
    printf ("Digite o codigo da carta:");
-   scanf ("%c", &codigo01);
+   scanf ("%s", &codigo01);
 
    printf ("Digite o nome da cidade:");
-   scanf ("%c"), &cidade01;
+   scanf ("%s"), &cidade01;
 
    printf ("Digite a quantidade da populacao:");
    scanf ("%d"), &populacao01;
