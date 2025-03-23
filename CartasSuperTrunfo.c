f948acff458b76322458ca6b6953dcfc05a3a130
robertapierre <202501066526@alunos.estacio.br>
2025-03-23 17:56:55 +0000

    em construcao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,9 @@ int main() {
    float area01;
    float pib01;
    int pontos_turisticos01;
+   float densidade01;
+   float percapita01;
+
 
 
    //Váriaveis para a segunda carta.
@@ -19,30 +22,41 @@ int main() {
    float area02;
    float pib02;
    int pontos_turisticos02;
+   float densidade02;
+   float percapita02;
+
 
    //Solicitação dos dados para o usuário, primeira carta.
    printf ("Digite os dados da primeira carta:");
 
-   printf ("Digite o nome do estado:");
+   printf ("Digite o nome do estado: \n");
    scanf ("%c", &estado01);
 
-   printf ("Digite o codigo da carta:");
+   printf ("Digite o codigo da carta:\n");
    scanf ("%s", &codigo01);
 
-   printf ("Digite o nome da cidade:");
-   scanf ("%s"), &cidade01;
+   printf ("Digite o nome da cidade:\n");
+   scanf ("%s", &cidade01);
 
-   printf ("Digite a quantidade da populacao:");
-   scanf ("%d"), &populacao01;
+   printf ("Digite a quantidade da populacao:\n");
+   scanf ("%d", &populacao01);
    
-   printf ("Digite a area em km²:");
-   scanf ("%f"), &area01;
+   printf ("Digite a area em km²:\n");
+   scanf ("%f", &area01);
 
-   printf ("Digite o Pib:");
-   scanf ("%f"), &pib01;
+   printf ("Digite o Pib:\n");
+   scanf ("%f", &pib01);
 
-   printf ("Digite a quantidade de pontos turisticos:");
-   scanf ("%d"), &pontos_turisticos01;
+   printf ("Digite a quantidade de pontos turisticos:\n");
+   scanf ("%d", &pontos_turisticos01);
+
+   printf ("Digite a densidade populacional:\n");
+   scanf("%f", &densidade01);
+
+   printf ("Digite o Pib Per Capita:\n")
+   scanf ("%f", &percapita01)
+
+   printf ()
    
 //Solicitação para o usuário, segunda carta.
 
